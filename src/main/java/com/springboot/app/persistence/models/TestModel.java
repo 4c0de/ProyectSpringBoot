bd2d376956149d5f78dd362cd04999189251acd8
Lenovo <Lenovo@LENOVO-T420>
2017-09-11 10:54:07 +0200

    new updates.

--- src/main/java/com/springboot/app/persistence/models/TestModel.java
+++ src/main/java/com/springboot/app/persistence/models/TestModel.java
@@ -1,18 +1,23 @@
 package com.springboot.app.persistence.models;
 
+/**
+ * Clase TestModel 
+ */
+
+
+
+
 
 public class TestModel
 {
-
+ //atributos
   private String table;
 
-
-
+  //getters and setters 
   public String getTable()
   {
     return table;
   }
-
 
   public void setTable(String table)
   {
--- src/main/java/com/springboot/app/services/TestServiceImpl.java
+++ src/main/java/com/springboot/app/services/TestServiceImpl.java
@@ -1,16 +1,21 @@
 package com.springboot.app.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.springboot.app.persistence.mappers.TestMapper;
 import com.springboot.app.persistence.models.TestModel;
+
 import java.util.List;
 
 
 @Service
 public class TestServiceImpl implements TestService
 {
-
+    
+     //inyectamos iMapper para utilizar los servicios implementados
+    
   @Autowired
   TestMapper iMapper;
 
@@ -19,7 +24,7 @@ public class TestServiceImpl implements TestService
   public List<TestModel> testService(TestModel obj) throws Exception
   {
     List<TestModel> x = iMapper.testMapper(obj);
-
+    
     return x;
   }
 
